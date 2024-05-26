02ba7c527254f8b5a2462c5c2c39a7ae145cd02c
ehsanfakhri <fakhri.ehsan@gmail.com>
2024-05-26 19:06:00 +0330

    some styles fix

--- components/headers/navbar/MobileNav.tsx
+++ components/headers/navbar/MobileNav.tsx
@@ -18,11 +18,13 @@ const MobileNav = () => {
           <div className="flex flex-col items-center gap-y-32">
             {/* sheet close */}
             <Logo />
-            <Nav
-              containerStyles="flex flex-col flex-col-reverse items-center gap-y-6"
-              LinkStyles="text-xl  p-4 px-8 w-full text-center dark:hover:bg-primary dark:bg-secondary 
-               hover:text-white bg-primary-foreground hover:bg-primary"
-            />
+            <div className="bg-primary dark:bg-secondary w-full rounded-sm p-4 ">
+              <Nav
+                containerStyles="flex flex-col flex-col-reverse items-center gap-y-6  "
+                LinkStyles="text-xl  p-4 px-8 w-full text-center rounded-full dark:hover:bg-primary dark:bg-secondary 
+               hover:text-black text-white bg-primary hover:bg-white"
+              />
+            </div>
           </div>
           <Socials containerStyles="flex gap-x-4" iconsStyles="text-2xl" />
         </div>
